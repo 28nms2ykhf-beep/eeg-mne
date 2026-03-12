d1d60e1ed4b2fa71d100173d78bb05e27053ee05
vincent shi <28nms2ykhf@privaterelay.appleid.com>
2026-03-12 13:03:41 +0000

    Update model.py

--- model.py
+++ model.py
@@ -2,14 +2,14 @@
 from torchegg.models import EEGNet
 
 def create_eegnet(chunk_size, num_electrodes, F1, F2, D, num_classes,
-                  kernal1, kernal2, dropout):
+                  kernel_1, kernel_2, dropout):
     model = EEGNet(chunk_size=chunk_size,
                    num_electrodes=num_electrodes,
                    F1=F1,
                    F2=F2,
                    D=D,
                    num_classes=num_classes,
-                   kernal1=kernal1,
-                   kernal2=kernal2,
+                   kernel_1=kernel_1,     
+                   kernel_2=kernel_2,      
                    dropout=dropout)
     return model
